93db994babbfcc5f9a8ce23a63e9e8666e8a58f4
Arthur Zaczek <mosdev@protonmail.com>
2021-06-14 12:01:26 +0200

    initial commit

---
+++ apps/client-pickjob/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'client-pickjob',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/client-pickjob',
+};
